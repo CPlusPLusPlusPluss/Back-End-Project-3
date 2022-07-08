78cab88fd89c4916e65b4119c0b400c3106d9848
nickgiggey <nickgiggey@comcast.net>
2022-07-08 10:51:50 -0600

    login added

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const {urlencoded} = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const dataController = require('./controllers/dataController')
-const userController = require('./controllers/userController')
+const { urlencoded } = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const dataController = require('./controllers/dataController');
+const userController = require('./controllers/userController');
 const app = express();
-const port = process.env.PORT || 3001
-const MONGOURI = process.env.MONGO_URI
-mongoose.connect(MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true})
+const port = process.env.PORT || 3001;
+const MONGOURI = process.env.MONGO_URI;
+mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: '));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-app.use(morgan('short'))
-app.use(cors())
+app.use(morgan('short'));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/datas', dataController)
-app.use('/users', userController)
+app.use('/datas', dataController);
+app.use('/users', userController);
 
 app.listen(port, () => {
-    console.log('app running...')
-})
+    console.log('app running...');
+});
